d7b52768e64ba94bec07cf605aa8f27a3cc342af
tejendrauppal <tejendrauppal@gmail.com>
2026-03-10 15:19:52 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100  flex items-center justify-center p-4">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hello Tailwind 🚀
+      </h1>
+
+      <h1 className="text-5xl text-red-500 font-bold">
+        Tailwind is Working
+      </h1>
+    </div>
+  );
+}
+
+export default App;
